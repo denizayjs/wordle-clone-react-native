11fb6aa830d6269dd1a240b84d7e9c98df7d607e
deniz ay <denizayjs@gmail.com>
2024-11-17 21:25:11 +0300

    feat: create menu page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,36 @@
 import { Stack } from 'expo-router';
+//import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import {
+  useFonts,
+  FrankRuhlLibre_500Medium,
+  FrankRuhlLibre_800ExtraBold,
+  FrankRuhlLibre_900Black,
+} from '@expo-google-fonts/frank-ruhl-libre';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    FrankRuhlLibre_500Medium,
+    FrankRuhlLibre_800ExtraBold,
+    FrankRuhlLibre_900Black,
+  });
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen name='index' />
+      <Stack.Screen name='index' options={{ headerShown: false }} />
     </Stack>
   );
 }
